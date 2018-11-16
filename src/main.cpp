2e2322df8fc31ff7aceeaa8ceec20b7a97921cca
Abdullah Kady <abdullahkady@gmail.com>
2018-11-16 13:00:41 +0200

    Add initial project structure

---
+++ src/main.cpp
@@ -0,0 +1,425 @@
+//	Includes
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <sstream>
+
+#define GLUT_KEY_ESCAPE 27
+#define DEG2RAD(a) (a * 0.0174532925)
+
+class Vector3f
+{
+public:
+	float x, y, z;
+
+	Vector3f(float _x = 0.0f, float _y = 0.0f, float _z = 0.0f)
+	{
+		x = _x;
+		y = _y;
+		z = _z;
+	}
+
+	Vector3f operator+(Vector3f const &v)
+	{
+		return Vector3f(x + v.x, y + v.y, z + v.z);
+	}
+
+	Vector3f operator-(Vector3f &v)
+	{
+		return Vector3f(x - v.x, y - v.y, z - v.z);
+	}
+
+	Vector3f operator*(float n)
+	{
+		return Vector3f(x * n, y * n, z * n);
+	}
+
+	Vector3f operator/(float n)
+	{
+		return Vector3f(x / n, y / n, z / n);
+	}
+
+	Vector3f unit()
+	{
+		return *this / sqrt(x * x + y * y + z * z);
+	}
+
+	Vector3f cross(Vector3f v)
+	{
+		return Vector3f(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x);
+	}
+};
+
+class Camera
+{
+public:
+	Vector3f eye, center, up;
+
+	Camera(float eyeX = 1.0f, float eyeY = 1.0f, float eyeZ = 1.0f, float centerX = 0.0f, float centerY = 0.0f, float centerZ = 0.0f, float upX = 0.0f, float upY = 1.0f, float upZ = 0.0f)
+	{
+		eye = Vector3f(eyeX, eyeY, eyeZ);
+		center = Vector3f(centerX, centerY, centerZ);
+		up = Vector3f(upX, upY, upZ);
+		Vector3f view = (center - eye).unit();
+		eye = eye + view * -0.7;
+		center = center + view * -0.7;
+	}
+
+	void moveX(float d)
+	{
+		Vector3f right = up.cross(center - eye).unit();
+		eye = eye + right * d;
+		center = center + right * d;
+	}
+
+	void moveY(float d)
+	{
+		eye = eye + up.unit() * d;
+		center = center + up.unit() * d;
+	}
+
+	void moveZ(float d)
+	{
+		Vector3f view = (center - eye).unit();
+		eye = eye + view * d;
+		center = center + view * d;
+	}
+
+	void rotateX(float a)
+	{
+		Vector3f view = (center - eye).unit();
+		Vector3f right = up.cross(view).unit();
+		view = view * cos(DEG2RAD(a)) + up * sin(DEG2RAD(a));
+		up = view.cross(right);
+		center = eye + view;
+	}
+
+	void rotateY(float a)
+	{
+		Vector3f view = (center - eye).unit();
+		Vector3f right = up.cross(view).unit();
+		view = view * cos(DEG2RAD(a)) + right * sin(DEG2RAD(a));
+		right = view.cross(up);
+		center = eye + view;
+	}
+
+	void look()
+	{
+		gluLookAt(
+				eye.x, eye.y, eye.z,
+				center.x, center.y, center.z,
+				up.x, up.y, up.z);
+	}
+};
+
+Camera camera;
+
+void setDefaultColor()
+{
+	glColor3f(.8, .8, .8);
+}
+
+void setupLights()
+{
+	GLfloat ambient[] = {0.7f, 0.7f, 0.7, 1.0f};
+	GLfloat diffuse[] = {0.6f, 0.6f, 0.6, 1.0f};
+	GLfloat specular[] = {1.0f, 1.0f, 1.0, 1.0f};
+	GLfloat shininess[] = {50};
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, specular);
+	glMaterialfv(GL_FRONT, GL_SHININESS, shininess);
+
+	GLfloat lightIntensity[] = {0.7f, 0.7f, 1, 1.0f};
+	// GLfloat lightPosition[] = { -7.0f, 6.0f,3.0f, 0.0f };
+	glLightfv(GL_LIGHT0, GL_POSITION, lightIntensity);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightIntensity);
+}
+
+void setupCamera()
+{
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(60, 640 / 480, 0.001, 100);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	camera.look();
+}
+
+void drawWall(double thickness)
+{
+	glPushMatrix();
+	glColor3f(0.8, 0.4, 0.1); // Brown
+	glTranslated(0.5, 0.5 * thickness, 0.5);
+	glScaled(1.0, thickness, 1.0);
+	glutSolidCube(1);
+	glPopMatrix();
+	setDefaultColor();
+}
+
+void drawTableLeg(double thick, double len)
+{
+	glPushMatrix();
+	glTranslated(0, len / 2, 0);
+	glScaled(thick, len, thick);
+	glutSolidCube(1.0);
+	glPopMatrix();
+}
+
+void drawJackPart()
+{
+	glPushMatrix();
+	glScaled(0.2, 0.2, 1.0);
+	glutSolidSphere(1, 15, 15);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslated(0, 0, 1.2);
+	glutSolidSphere(0.2, 15, 15);
+	glTranslated(0, 0, -2.4);
+	glutSolidSphere(0.2, 15, 15);
+	glPopMatrix();
+}
+
+void drawJack()
+{
+	glPushMatrix();
+	drawJackPart();
+	glRotated(90.0, 0, 1, 0);
+	drawJackPart();
+	glRotated(90.0, 1, 0, 0);
+	drawJackPart();
+	glPopMatrix();
+}
+
+void drawTable(double topWid, double topThick, double legThick, double legLen)
+{
+	glPushMatrix();
+	glTranslated(0, legLen, 0);
+	glScaled(topWid, topThick, topWid);
+	glutSolidCube(1.0);
+	glPopMatrix();
+
+	double dist = 0.95 * topWid / 2.0 - legThick / 2.0;
+	glPushMatrix();
+	glTranslated(dist, 0, dist);
+	drawTableLeg(legThick, legLen);
+	glTranslated(0, 0, -2 * dist);
+	drawTableLeg(legThick, legLen);
+	glTranslated(-2 * dist, 0, 2 * dist);
+	drawTableLeg(legThick, legLen);
+	glTranslated(0, 0, -2 * dist);
+	drawTableLeg(legThick, legLen);
+	glPopMatrix();
+}
+
+void drawFirstRoom()
+{
+	glPushMatrix();
+	glScaled(0.5, 0.5, 0.5);
+	glPushMatrix();
+	glTranslated(0.4, 0.4, 0.6);
+	glRotated(45, 0, 0, 1);
+	glScaled(0.08, 0.08, 0.08);
+	drawJack();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslated(0.6, 0.38, 0.5);
+	glRotated(30, 0, 1, 0);
+	glutSolidTeapot(0.08);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslated(0.25, 0.42, 0.35);
+	glutSolidSphere(0.1, 15, 15);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslated(0.4, 0.0, 0.4);
+	drawTable(0.6, 0.02, 0.02, 0.3);
+	glPopMatrix();
+
+	glPopMatrix();
+
+	// ============ WALLS ============ //
+	drawWall(0.02);
+	glPushMatrix();
+	glRotated(90, 0, 0, 1.0);
+	drawWall(0.02);
+	glPopMatrix();
+	glPushMatrix();
+	glRotated(-90, 1.0, 0.0, 0.0);
+	drawWall(0.02);
+	glPopMatrix();
+	// ============ WALLS ============ //
+}
+
+void drawSecondRoom()
+{
+
+	// Back wall
+	glPushMatrix();
+	glRotated(-90, 1.0, 0.0, 0.0);
+	glRotated(-90, 0.0, 1.0, 0.0);
+	drawWall(0.02);
+	glPopMatrix();
+
+	// Floor
+	glPushMatrix();
+	glTranslated(-1.0, 0.0, 0.0);
+	drawWall(0.02);
+	glPopMatrix();
+}
+
+void Display()
+{
+	setupCamera();
+	setupLights();
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	drawFirstRoom();
+	drawSecondRoom();
+
+	glFlush();
+}
+
+void setLeftView()
+{
+	camera.up.x = -0.00405836;
+	camera.up.y = 0.999989;
+	camera.up.z = 0.00230124;
+
+	camera.center.x = -0.713009;
+	camera.center.y = 0.448415;
+	camera.center.z = 0.426624;
+
+	camera.eye.x = -1.71246;
+	camera.eye.y = 0.444435;
+	camera.eye.z = 0.393797;
+}
+
+void setTopView()
+{
+	camera.up.x = 0.0226687;
+	camera.up.y = -0.0143616;
+	camera.up.z = -0.99964;
+
+	camera.center.x = -0.00778855;
+	camera.center.y = 1.79373;
+	camera.center.z = 0.516674;
+
+	camera.eye.x = -0.00758377;
+	camera.eye.y = 2.79363;
+	camera.eye.z = 0.502314;
+}
+
+void setRightView()
+{
+	camera.up.x = -0.108454;
+	camera.up.y = 0.994058;
+	camera.up.z = -0.00928754;
+
+	camera.center.x = 0.952321;
+	camera.center.y = 0.526236;
+	camera.center.z = 0.454693;
+
+	camera.eye.x = 1.94623;
+	camera.eye.y = 0.634482;
+	camera.eye.z = 0.434158;
+}
+
+void onKeyPress(unsigned char key, int x, int y)
+{
+	float d = 0.01 * 10;
+
+	switch (key)
+	{
+	case 'w':
+		camera.moveY(d);
+		break;
+	case 's':
+		camera.moveY(-d);
+		break;
+	case 'a':
+		camera.moveX(d);
+		break;
+	case 'd':
+		camera.moveX(-d);
+		break;
+	case 'q':
+		camera.moveZ(d);
+		break;
+	case 'e':
+		camera.moveZ(-d);
+		break;
+	case 'h':
+		setRightView();
+		break;
+	case 'g':
+		setLeftView();
+		break;
+	case 'y':
+		setTopView();
+		break;
+	case GLUT_KEY_ESCAPE:
+		exit(EXIT_SUCCESS);
+	}
+
+	glutPostRedisplay();
+}
+
+void onSpecialKey(int key, int x, int y)
+{
+	float a = 3.0;
+
+	switch (key)
+	{
+	case GLUT_KEY_UP:
+		camera.rotateX(a);
+		break;
+	case GLUT_KEY_DOWN:
+		camera.rotateX(-a);
+		break;
+	case GLUT_KEY_LEFT:
+		camera.rotateY(a);
+		break;
+	case GLUT_KEY_RIGHT:
+		camera.rotateY(-a);
+		break;
+	}
+
+	glutPostRedisplay();
+}
+
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+
+	glutInitWindowSize(640, 480);
+	glutInitWindowPosition(50, 50);
+
+	glutCreateWindow("3d-house-scene");
+	glutDisplayFunc(Display);
+	glutKeyboardFunc(onKeyPress);
+	glutSpecialFunc(onSpecialKey);
+
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+	glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_NORMALIZE);
+	glEnable(GL_COLOR_MATERIAL);
+
+	glShadeModel(GL_SMOOTH);
+
+	glutMainLoop();
+	return 0;
+}
